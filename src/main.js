1da79c3e69ff388a9d58344a3d7929f585b9922c
Roberto Rodríguez Bqz <105755674+RobertoRodriguezBorquez@users.noreply.github.com>
2025-01-25 11:45:44 -0300

    busqueda funcional , subrayando las palabras encontradas. falta dar el ultimo estilo a la pag

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
 /* Capturar texto de bienvenida. */
 const textEntrada = document.getElementById("textoEntrada");
+//actalizamos texto
 textEntrada.innerText =
   "Bienvenido, porfavor introduzca la palabra a buscar en el siguiente en el siguiente texto";
 
 /* Capturar buscador */
 const buscador = document.getElementById("inputBuscador");
 
-/* almacenar la id de la etiqueta que contiene el texto */
+/* almacenar la id de la etiqueta que mostrara la palabra que se introdujo a buscar.*/
 let resultado = document.getElementById("resultadoo");
+
 /* Se declara la variable que almacenará el texto de la de la etiqueta con la id resultadoo */
-let buscarPalabra="";
+let buscarPalabra = "";
 
-
-/* capturar etiqueta que tiene el texto  */
+/* capturar etiqueta que tiene el texto parrafo  */
 const textoParrafo = document.getElementById("texto-parrafo");
 
 /* evento  para guardar en resultado el input */
 buscador.addEventListener("input", (event) => {
   resultado.innerText = event.target.value;
-   buscarPalabra=resultado.innerText
+  buscarPalabra = resultado.innerText;
 
   console.log(`la letra o palabra a buscar es: ${buscarPalabra}`);
 
@@ -26,20 +27,21 @@ buscador.addEventListener("input", (event) => {
   buscarPalabraEnTexto(textoParrafo, buscarPalabra);
 });
 
-
-
 const buscarPalabraEnTexto = (textoParrafo, buscarPalabra) => {
-console.log(textoParrafo.innerText, "-----imprimo el texto-----");
+  console.log(textoParrafo.innerText, "-----imprimo el texto-----");
 
-if (textoParrafo.innerText.includes(buscarPalabra)) {
-  console.log("Si,  se encuentra la palabra en el parrafo");
+  // Crear la expresión regular global e insensible a mayúsculas
+  const regex = new RegExp(`\\b${buscarPalabra}\\b`, "gi");
 
-}else{
+  if (textoParrafo.innerText.match(regex)) {
+    console.log("Si,  se encuentra la palabra en el parrafo");
+    console.log(textoParrafo.innerText.match(regex));
+
+    textoParrafo.innerHTML = textoParrafo.innerText.replace(
+      regex,
+      (match) => `<span style="background-color: yellow;">${match}</span>`
+    );
+  } else {
     console.log("nose encuentra la palabra:", buscarPalabra);
-    
-    
   }
-  
-
 };
-
